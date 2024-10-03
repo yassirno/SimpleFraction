76607ac63c7a7539fd0fe37360525f6a10caf8dc
yassir nouri <yassirnouri00@gmail.com>
2024-10-03 11:39:34 +0200

    v3

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import fraction.Fraction;
+public class Main {
+    public static void main(String[] args) {
+        Fraction f=new Fraction();
+        assert f.toString().compareTo("je suis une fraction")==0:
+        "Erreur";   
+    }
+    
+}
---
+++ src/fraction/Fraction.java
@@ -0,0 +1,9 @@
+package fraction;
+public class Fraction {
+    public String toString(){
+        return "je suis une fraction";
+    }
+    public Fraction (){
+        
+    }
+}
